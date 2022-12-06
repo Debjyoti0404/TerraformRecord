375e47a3bfac2f0cca80fd475d060334bfedafa0
CrashOverrider <91897336+Sankhadip-Roy@users.noreply.github.com>
2022-12-06 06:14:59 +0530

    Create ComputeInstance.tf

---
+++ ComputeInstance.tf
@@ -0,0 +1,36 @@
+# Infrastructure as Code with Terraform
+# creating computer instance
+# terraform --version
+# mkdir <dir>
+# touch <file.tf>
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_instance" "terraform" {
+  name         = "terraform"
+  machine_type = "n1-standard-2"
+  tags         = ["web", "dev"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+  allow_stopping_for_update = true #for updating vm config
+}
+# terraform init
+# terraform plan
+# terraform apply
+# terraform destroy
